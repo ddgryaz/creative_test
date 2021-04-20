5a3c5e3176d4ebfd6dea9f3d34bfce93ea4bcd13
Dmitriy Dzyuman <dzuman_dv@smart-transport.ru>
2021-04-20 10:05:46 +0500

    add new branch

---
+++ jun1/src/js/menu.js
@@ -0,0 +1,7 @@
+function menuSlide() {    
+    if (document.getElementById("menu").style.width == "30vw") {
+        document.getElementById("menu").style.width = "0vw"
+    } else {
+        document.getElementById("menu").style.width = "30vw"
+    }
+}
