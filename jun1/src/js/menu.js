8bce34949baab92b451e7ccc2cda064dd8204523
ddgryaz <dzuman1996@mail.ru>
2021-04-20 20:42:02 +0500

    Added functionality and filled the page

--- jun1/src/js/menu.js
+++ jun1/src/js/menu.js
@@ -1,7 +1,24 @@
-function menuSlide() {    
-    if (document.getElementById("menu").style.width == "30vw") {
-        document.getElementById("menu").style.width = "0vw"
+let size_control;
+
+window.addEventListener('resize', function () {
+    size_control = document.documentElement.clientWidth
+
+    if (size_control < 1200) {
+        document.getElementById("menu").style.width = "0vw";
+        document.getElementById('menu__elements').style.display = 'none'
     } else {
-        document.getElementById("menu").style.width = "30vw"
+        document.getElementById("menu").style.width = "30vw";
+        document.getElementById('menu__elements').style.display = 'block'
+    }
+});
+
+function menuSlide() {
+    let x = document.getElementById('menu__elements');
+    if (document.getElementById("menu").style.width == "30vw") {
+        document.getElementById("menu").style.width = "0vw";
+        document.getElementById('menu__elements').style.display = 'none'
+    } else {
+        document.getElementById("menu").style.width = "30vw";
+        document.getElementById('menu__elements').style.display = 'block'
     }
 }
